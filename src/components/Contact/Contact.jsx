11f691f812b6287bb4c0d648db9b03365c411c3f
Ibrahim Mabrouki <ibrahim.mabrouki@lau.edu>
2026-02-01 17:10:20 +0200

    images updates 1.0

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from './Contact.module.css';
 import emailicon from "../../assets/contact/emailIcon.png";
-import linkedinicon from "../../assets/contact/linkedinicon.png";
-import githubicon from "../../assets/contact/githubicon.png";
+import linkedinicon from "../../assets/contact/linkedinIcon.png";
+import githubicon from "../../assets/contact/githubIcon.png";
 
 
 
